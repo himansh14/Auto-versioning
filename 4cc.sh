706a39288a9e13a4c560d149bb03a28d2dd8b5c0
root <root@ip-172-31-45-110.ap-south-1.compute.internal>
2023-03-30 10:45:35 +0000

    latest commit to the file

---
+++ 4cc.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#add a change in the working directory to the staging area
+git add "."
+
+#commit the files and give message
+git commit -m "latest commit to the file"
+
+#git branching specify
+git branch -M main
+
+#configuration file settings
+#git commit config --global user.name "himansh14"
+#git commit config --global user.email "972214himanshu@gmail.com"
+git commit --amend --author="himansh14 972214himanshu@gmail.com" --no-edit
+
+#checking the current tag of the file
+NEW_TAG=$(git describe --tags --abbrev=0) 
+
+#latest tag on the current branch
+#LATEST_TAG=$(git describe --tags --abbrev=0 | awk -F. '{OFS="."; $NF+=1; print $0}')
+#LATEST_TAG=$(echo git describe --tags --abbrev=0 | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}') 
+
+#different versions for major minor patch
+MAJOR=$(echo "$NEW_TAG" | awk -F. '{print $1}')
+MINOR=$(echo "$NEW_TAG" | awk -F. '{print $2}')
+PATCH=$(echo "$NEW_TAG" | awk -F. '{print $3}')
+
+#determine the new tag number(different versions for major minor patch)
+
+case "$1" in
+	"MAJOR") ((major++)); minor=0; patch=0;; 
+        "MINOR") ((minor++)); patch=0;; 
+        "PATCH") ((patch++));; 
+	*) echo "Usage: $0 [major|minor|patch]"; exit 1;; 
+esac
+}
+
+#creating the new tag for versioning
+NEWEST_TAG="$MAJOR.$MINOR.$PATCH"
+echo "$NEWEST_TAG"
+
+#specifying new tags to the existing tags
+git tag -a "$NEWEST_TAG" -m "Version $NEWEST_TAG"
+
+
+#push changes to the remote
+git push origin "$NEWEST_TAG"
+
+#echo "$GIT_PUSH" > /opt/all_data_backup/backup/autoversion_script/version/version.txt 
---
+++ bash.sh
@@ -0,0 +1,20 @@
+increment_version ()
+{
+  declare -a part=( ${1//\./ } )
+  declare    new
+  declare -i carry=1
+
+  for (( CNTR=${#part[@]}-1; CNTR>=0; CNTR-=1 )); do
+    len=${#part[CNTR]}
+    new=$((part[CNTR]+carry))
+    [ ${#new} -gt $len ] && carry=1 || carry=0
+    [ $CNTR -gt 0 ] && part[CNTR]=${new: -len} || part[CNTR]=${new}
+  done
+  new="${part[*]}"
+  echo -e "${new// /.}"
+}
+
+AUTO_VERSIONING=$(echo git tag | awk -F. -v OFS=. '{$NF += 1 ; print}')
+
+increment_version $AUTO_VERSIONING
+
